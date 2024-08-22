81a83ef495093c71a4970113aba96c3e35bfdb41
devcci <jdj5250@gmail.com>
2024-08-23 03:06:08 +0900

    chore: Redis Test Container 준비 + 외부서버 연결용 .env

--- devtoy-product/src/test/java/com/devcci/devtoy/product/config/TestContainerConfig.java
+++ devtoy-product/src/test/java/com/devcci/devtoy/product/config/TestContainerConfig.java
@@ -3,6 +3,7 @@ package com.devcci.devtoy.product.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration
@@ -10,11 +11,18 @@ public class TestContainerConfig {
 
 
     @Bean
-    @ServiceConnection
+    @ServiceConnection("mysql")
     public MySQLContainer<?> mysqlContainer() {
         return new MySQLContainer<>("mysql:8.4.2")
             .withDatabaseName("devtoy-product-test")
             .withUsername("test")
             .withPassword("test");
+    }
+
+    @Bean
+    @ServiceConnection("redis")
+    public GenericContainer<?> redisContainer() {
+        return new GenericContainer<>("redis:7.4")
+            .withExposedPorts(6379);
     }
 }
