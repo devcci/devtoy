4db68fce38fbe5d10da1fc84df5d21df5a1ab137
devcci <jdj5250@gmail.com>
2024-10-26 17:08:25 +0900

    doc: gateway에서 자동 설정되는 header 값을 swagger에서 표시하지 않도록 수정

--- devtoy-gateway/src/main/java/com/devcci/devtoy/gateway/config/SwaggerConfig.java
+++ devtoy-gateway/src/main/java/com/devcci/devtoy/gateway/config/SwaggerConfig.java
@@ -2,7 +2,11 @@ package com.devcci.devtoy.gateway.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +14,17 @@ import org.springframework.context.annotation.Configuration;
     version = "v1.0.0"))
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        return new OpenAPI()
+            .components(new Components())
+            .addServersItem(new Server().url("/"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
